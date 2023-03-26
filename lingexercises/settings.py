c4af74d96658a3b0f03c39f93e22136787084540
Tony Linz <tonyllinz@gmail.com>
2023-03-25 17:47:38 -0700

    allow all hosts

--- lingexercises/settings.py
+++ lingexercises/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%ku83sprjejy!mu$2ca+s481d4do@tmj4e%+xs63dz+7vs#9yz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
